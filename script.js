f0018baab87ec0994ae48596e96cceac5199f680
Roman <romannadopta@g.mail.com>
2020-04-06 16:56:39 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let money = prompt ("Ваш бюджет на месяц?", ''),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", '');
+
+    let appData = {
+      butget: money,
+      expenses: {},
+      optionalExpenses:[],
+      income:[],
+      savings: false
+    };
+
+    let q1 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
+         q2 = prompt("Во сколько обойдется", ''),
+         q3 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
+         q4 = prompt("Во сколько обойдется", '');
+      appData.expenses[q1] = q2;
+      appData.expenses[q3] = q4;
+      alert(appData.butget / 30);
