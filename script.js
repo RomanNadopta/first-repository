242f48b8d55a1e623ddc60b946c87137b0725386
Roman <romannadopta@g.mail.com>
2020-04-08 12:19:54 +0300

    my third commit

--- script.js
+++ script.js
@@ -16,13 +16,49 @@ for (let i = 0; i < 2; i++) {
          
          appData.expenses[a] = b;
          if ((typeof(a))=== 'string' && (typeof(a)) != null && 
-         (typeof(b)) != null && a != '' && b != '') {
+         (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
           console.log('done');
           appData.expenses[a] = b;
+         } else {
+           console.log("bad results");
+           i--;
          }
 }
+// let i = 0;
 
-appData.moneyPerDay = appData.budget / 30;
+// while ( i < 2) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//          b = prompt("Во сколько обойдется", '');
+//   appData.expenses[a] = b;
+//          if ((typeof(a))=== 'string' && (typeof(a)) != null && 
+//          (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//           console.log('done');
+//           appData.expenses[a] = b;
+//          } 
+//          else {
+//           console.log("bad results");
+//           i--;
+//         }
+//         i++;
+// }
+// let i = 0;
+//          do {
+//           let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//                b = prompt("Во сколько обойдется", '');
+//           appData.expenses[a] = b;
+//                    if ((typeof(a))=== 'string' && (typeof(a)) != null && 
+//                    (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//                     console.log('done');
+//                     appData.expenses[a] = b;
+//                    } else {
+//                               console.log("bad results");
+//                               i--;
+//                             }
+//                             i++; 
+//          } 
+//          while (i < 2);
+
+ appData.moneyPerDay = appData.budget / 30;
  alert("everyday budjet" + appData.moneyPerDay);
 
  if(appData.moneyPerDay < 100) {
