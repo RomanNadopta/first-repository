f22885a10fb49766ddf8e81d99336e0251bc6be0
Roman <romannadopta@g.mail.com>
2020-04-10 13:45:46 +0300

    My fourth commit

--- script.js
+++ script.js
@@ -1,67 +1,49 @@
-let money = +prompt ("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start() {
+   money = +prompt ("Ваш бюджет на месяц?", '');
     time = prompt ("Введите дату в формате YYYY-MM-DD", '');
+    
+    while(isNaN(money) || money =="" || money == null) {
+      money = +prompt ("Ваш бюджет на месяц?", '');
+    }
+}
+start();
 
     let appData = {
       budget: money,
       expenses: {},
       optionalExpenses: {},
       income:[],
-      savings: false
+      savings: true
     };
 
+    function chooseExpenses() {
+      for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+               b = prompt("Во сколько обойдется", '');
+              
+               if ((typeof(a))=== 'string' && (typeof(a)) != null && 
+               (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+                console.log('done');
+                appData.expenses[a] = b;
+               } else {
+                 console.log("bad results");
+                 i = i - 1;
+               }
+      }
+    }
+chooseExpenses();
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-         b = prompt("Во сколько обойдется", '');
-         
-         appData.expenses[a] = b;
-         if ((typeof(a))=== 'string' && (typeof(a)) != null && 
-         (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-          console.log('done');
-          appData.expenses[a] = b;
-         } else {
-           console.log("bad results");
-           i--;
-         }
-}
-// let i = 0;
-
-// while ( i < 2) {
-//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-//          b = prompt("Во сколько обойдется", '');
-//   appData.expenses[a] = b;
-//          if ((typeof(a))=== 'string' && (typeof(a)) != null && 
-//          (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//           console.log('done');
-//           appData.expenses[a] = b;
-//          } 
-//          else {
-//           console.log("bad results");
-//           i--;
-//         }
-//         i++;
-// }
-// let i = 0;
-//          do {
-//           let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-//                b = prompt("Во сколько обойдется", '');
-//           appData.expenses[a] = b;
-//                    if ((typeof(a))=== 'string' && (typeof(a)) != null && 
-//                    (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//                     console.log('done');
-//                     appData.expenses[a] = b;
-//                    } else {
-//                               console.log("bad results");
-//                               i--;
-//                             }
-//                             i++; 
-//          } 
-//          while (i < 2);
-
- appData.moneyPerDay = appData.budget / 30;
+function detectDayBudget() {
+ appData.moneyPerDay = (appData.budget / 30).toFixed(); 
+ 
  alert("everyday budjet" + appData.moneyPerDay);
+}
+detectDayBudget();
 
- if(appData.moneyPerDay < 100) {
+function detectLevel () {
+if(appData.moneyPerDay < 100) {
   console.log("минимальный  уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 200) {
 console.log("medium level rich");
@@ -70,3 +52,32 @@ console.log("medium level rich");
 } else {
 console.log("Error");
 }
+}
+detectLevel();
+
+function chooseOptExspenses () {
+  for (let i = 1; i <= 3; i++) {
+    let questionOptExpenses = prompt("Статья необязательных расходов?");
+    appData.optionalExpenses[i] = questionOptExpenses;
+    console.log(appData.optionalExpenses);
+  }
+}
+
+chooseOptExspenses ();
+
+
+
+function checkSavings() {
+  if(appData.savings == true) {
+    let save = +prompt("How amount accumulation?"),
+    percent = +prompt("At what percentage?");
+
+    appData.monthIncome = save/100/12*percent;
+    alert("Income per month from your deposit" + appData.monthIncome);
+  }
+}
+
+checkSavings();
+
+
+
